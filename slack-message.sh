063cbffeddbcf40b389dea2bf5ea6cf5a77e9b7b
cyber alchemist <talk2bruce@makeralchemy.com>
2023-03-18 21:16:05 -0700

    updated comments in slack-message.sh
    added comment about where in the script to add slack webhook credentials

--- slack-message.sh
+++ slack-message.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # this file should be stored in /home/pi/.octoprint/slack-message.sh
+# replace AAAAAAAAA/BBBBBBBBB/CCCCCCCCCCCCCCCCCCCCCCCC with your slack webhook credentials
 curl -d "{'text': '$1'}" -H "Content-Type: application/json" https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBBB/CCCCCCCCCCCCCCCCCCCCCCCC
